dd383a8ee7e6e2cc09a748db3e01620607fe93b2
Pulindu1 <cfcf66@durham.ac.uk>
2025-07-07 12:57:21 +0100

    changed title and added footer links

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,7 +1,13 @@
 function Footer(){
     return(
         <footer>
-            <p>&copy; {new Date().getFullYear()} Your website name</p>
+            <p>&copy; {new Date().getFullYear()} Durham University Real Estate Finance and Investment Society</p>
+            <p>Contact us</p>
+            <ul>
+                <li><a href="mailto:durefis@durham.ac.uk">durefis@durham.ac.uk</a></li>
+                <li><a href="https://www.linkedin.com/company/durefis/posts/?feedView=all">LinkedIn</a></li>
+                <li><a href="https://www.instagram.com/durefissoc?igsh=MTE5azhiemRlZXV2OQ==">Instagram</a></li>
+            </ul>
         </footer>
     );
 
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return(
         <header>
-            <h1>My Website</h1>
+            <h1>Durham University Real Estate Finance and Investment Society</h1>
             <nav>
                 <ul>
                     <li><a href="#">Home</a></li>
